014aaff5977946730e41e7ee55ad683c666624ca
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2021-08-20 01:17:19 -0400

    setting up the proper type inheritance within win32

--- lcl/interfaces/win32/win32wsextctrls.pp
+++ lcl/interfaces/win32/win32wsextctrls.pp
@@ -33,108 +33,108 @@ uses
   ExtCtrls, Controls, ImgList, LCLType, LCLIntf, LazUTF8, Themes, LCLMessageGlue, ComCtrls, WSComCtrls,
 // ws
   WSControls, WSExtCtrls, {$ifdef wsintf}Graphics, WSLCLClasses_Intf{$else}WSLCLClasses{$endif}, WSProc, Win32Extra, Win32Int, Win32Proc,
-  InterfaceBase, Win32WSControls;
+  InterfaceBase, Win32WSControls{$ifdef wsintf}, Win32WSStdCtrls{$endif};
 
 type
   { TWin32WSPage }
 
-  TWin32WSPage = class(TWSPage)
+  TWin32WSPage = class({$ifndef wsintf}TWSPage{$else}TWin32WSCustomControl{$endif})
   published
   end;
 
   { TWin32WSNotebook }
 
-  TWin32WSNotebook = class(TWSNotebook)
+  TWin32WSNotebook = class({$ifndef wsintf}TWSNotebook{$else}TWin32WSCustomControl{$endif})
   published
   end;
 
   { TWin32WSShape }
 
-  TWin32WSShape = class(TWSShape)
+  TWin32WSShape = class({$ifndef wsintf}TWSShape{$else}TWin32WSControl{$endif})
   published
   end;
 
   { TWin32WSCustomSplitter }
 
-  TWin32WSCustomSplitter = class(TWSCustomSplitter)
+  TWin32WSCustomSplitter = class({$ifndef wsintf}TWSCustomSplitter{$else}TWin32WSCustomControl{$endif})
   published
   end;
 
   { TWin32WSSplitter }
 
-  TWin32WSSplitter = class(TWSSplitter)
+  TWin32WSSplitter = class({$ifndef wsintf}TWSSplitter{$else}TWin32WSCustomControl{$endif})
   published
   end;
 
   { TWin32WSPaintBox }
 
-  TWin32WSPaintBox = class(TWSPaintBox)
+  TWin32WSPaintBox = class({$ifndef wsintf}TWSPaintBox{$else}TWin32WSControl{$endif})
   published
   end;
 
   { TWin32WSCustomImage }
 
-  TWin32WSCustomImage = class(TWSCustomImage)
+  TWin32WSCustomImage = class({$ifndef wsintf}TWSCustomImage{$else}TWin32WSControl{$endif})
   published
   end;
 
   { TWin32WSImage }
 
-  TWin32WSImage = class(TWSImage)
+  TWin32WSImage = class({$ifndef wsintf}TWSImage{$else}TWin32WSCustomImage{$endif})
   published
   end;
 
   { TWin32WSBevel }
 
-  TWin32WSBevel = class(TWSBevel)
+  TWin32WSBevel = class({$ifndef wsintf}TWSBevel{$else}TWin32WSControl{$endif})
   published
   end;
 
   { TWin32WSCustomRadioGroup }
 
-  TWin32WSCustomRadioGroup = class(TWSCustomRadioGroup)
+  TWin32WSCustomRadioGroup = class({$ifndef wsintf}TWSCustomRadioGroup{$else}TWin32WSCustomGroupBox{$endif})
   published
   end;
 
   { TWin32WSRadioGroup }
 
-  TWin32WSRadioGroup = class(TWSRadioGroup)
+  TWin32WSRadioGroup = class({$ifndef wsintf}TWSRadioGroup{$else}TWin32WSCustomRadioGroup{$endif})
   published
   end;
 
   { TWin32WSCustomCheckGroup }
 
-  TWin32WSCustomCheckGroup = class(TWSCustomCheckGroup)
+  TWin32WSCustomCheckGroup = class({$ifndef wsintf}TWSCustomCheckGroup{$else}TWin32WSCustomGroupBox{$endif})
   published
   end;
 
   { TWin32WSCheckGroup }
 
-  TWin32WSCheckGroup = class(TWSCheckGroup)
+  TWin32WSCheckGroup = class({$ifndef wsintf}TWSCheckGroup{$else}TWin32WSCustomCheckGroup{$endif})
   published
   end;
 
   { TWin32WSCustomLabeledEdit }
 
-  TWin32WSCustomLabeledEdit = class(TWSCustomLabeledEdit)
+  TWin32WSCustomLabeledEdit = class({$ifndef wsintf}TWSCustomLabeledEdit{$else}TWin32WSCustomEdit{$endif})
   published
   end;
 
   { TWin32WSLabeledEdit }
 
-  TWin32WSLabeledEdit = class(TWSLabeledEdit)
+  TWin32WSLabeledEdit = class({$ifndef wsintf}TWSLabeledEdit{$else}TWin32WSCustomLabeledEdit{$endif})
   published
   end;
 
   { TWin32WSCustomPanel }
 
-  TWin32WSCustomPanel = class(TWSCustomPanel)
+  TWin32WSCustomPanel = class({$ifndef wsintf}TWSCustomPanel{$else}TWin32WSCustomControl{$endif})
   published
   end;
 
   { TWin32WSPanel }
 
-  TWin32WSPanel = class(TWSPanel)
+  TWin32WSPanel = class({$ifndef wsintf}TWSPanel{$else}TWin32WSCustomPanel{$endif})
   published
   end;
 
